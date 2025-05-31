3374346ecc2325bcc7c58ef8ec8d1950b0cdb2ab
Prashant Kamboj <prashant.kamboj4@gmail.com>
2025-06-01 02:55:45 +0530

    my change

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -13,7 +13,7 @@ export const Home = () => {
           </h1>
 
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            I’m a full-stack developer who loves crafting clean, scalable web
+            I’m a frontend developer who loves crafting clean, scalable web
             applications. My goal is to build solutions that offer both
             exceptional performance and a delightful user experience.
           </p>
